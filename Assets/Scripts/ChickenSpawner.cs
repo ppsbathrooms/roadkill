361055943bdf186043c48d7ca7ed12b5f773f055
finnmprice <finnprice03@gmail.com>
2023-06-01 13:49:15 -0700

    chicken spawn fix

--- Assets/Scripts/ChickenSpawner.cs
+++ Assets/Scripts/ChickenSpawner.cs
@@ -8,18 +8,24 @@ public class ChickenSpawner : MonoBehaviour
     [SerializeField] private Vector3 boundsMin;
     [SerializeField] private Vector3 boundsMax;
 
+    [SerializeField] private float maxChickens;
+
     [SerializeField] private float spawnTime;
-    
+    private GameObject scene;
+
     private void Start()
     {
+        scene = GameObject.Find("Scene");
         InvokeRepeating(nameof(SpawnChicken), spawnTime, spawnTime);
     }
 
     private void SpawnChicken()
     {
-        Vector3 pos = new Vector3(Random.Range(boundsMin.x, boundsMax.x), Random.Range(boundsMin.y, boundsMax.y),
-            Random.Range(boundsMin.z, boundsMax.z));
+        if(scene.transform.hierarchyCount < maxChickens) {
+            Vector3 pos = new Vector3(Random.Range(boundsMin.x, boundsMax.x), Random.Range(boundsMin.y, boundsMax.y),
+                Random.Range(boundsMin.z, boundsMax.z));
 
-        Instantiate(chicken, pos, Quaternion.Euler(-90f, 0f, 0f), Settings.instance.chickenContainer);
+            Instantiate(chicken, pos, Quaternion.Euler(-90f, 0f, 0f), Settings.instance.chickenContainer);
+        }
     }
 }
