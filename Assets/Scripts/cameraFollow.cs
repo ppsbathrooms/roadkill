1d896f9716eeb3ad267315dd83d365dff01526e3
Finn <finnprice03@gmail.com>
2023-08-09 23:42:57 -0700

    camera wont clip through objects in scene

--- Assets/Scripts/cameraFollow.cs
+++ Assets/Scripts/cameraFollow.cs
@@ -1,31 +1,34 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
     [Header("Settings")]
     [SerializeField] private Vector3 offset;
-    [SerializeField] private Transform target;
     [SerializeField] private float translateSpeed;
     [SerializeField] private float rotationSpeedX;
     [SerializeField] private float rotationSpeedY;
+
+    [Space][Header("Refs")]
+    [SerializeField] private Transform target;
+
     
-    private float yMin = -20f;
-    private float yMax = 60f;
+    private float yMin = -25f;
+    private float yMax = 90f;
     private float yRotation;
 
-
+    private float backDistance;
+    private float cameraOffset = 0f;
 
     void Update()
     {
         yRotation += -Input.GetAxis("Mouse Y") * rotationSpeedY;
         yRotation = Mathf.Clamp(yRotation, yMin, yMax);
-
+        RayGroundCollision();
     }
+
     private void FixedUpdate()
     {
-        var targetPosition = target.TransformPoint(offset);
+        var targetPosition = target.TransformPoint(offset.x  - cameraOffset, offset.y, offset.z);
         transform.position = Vector3.Slerp(transform.position, targetPosition, translateSpeed * Time.fixedDeltaTime);
 
         var direction = target.position - transform.position;
@@ -33,5 +36,37 @@ public class CameraFollow : MonoBehaviour
 
         transform.rotation = Quaternion.Slerp(transform.rotation, rotation, rotationSpeedX * Time.fixedDeltaTime);
         target.transform.localRotation = Quaternion.Euler(0, 90, yRotation);
+    }
+
+    private void RayGroundCollision()
+    {
+        int groundMask = 1 << 3;
+        float distance = Vector3.Distance(transform.position, target.position);
+        Vector3 rayEnding = Camera.main.ViewportToWorldPoint (new Vector3(0.5f, 0.5f, 0.0f));
+        RaycastHit hit;
+        RaycastHit backHit;
+        
+        Vector3 dir = rayEnding - target.position;
+        Debug.DrawRay(target.position, dir, Color.red);
+
+        //going back raycast
+        if (Physics.Raycast (target.position, dir, out backHit, 10f, groundMask))
+        {
+            backDistance = backHit.distance;
+        }
+
+        if (Physics.Raycast (target.position, dir, out hit, distance, groundMask))
+        {
+            if(cameraOffset < 1.15)
+            {                
+                cameraOffset = Mathf.Lerp(cameraOffset, cameraOffset + (distance - hit.distance), Time.fixedDeltaTime * 10f);
+            }
+        }
+        else
+        {
+            if(cameraOffset > 0) {               
+                cameraOffset = Mathf.Lerp(cameraOffset, cameraOffset - (backDistance - distance), Time.fixedDeltaTime * 3f);
+            }
+        }
     }
 }
