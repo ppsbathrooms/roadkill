b42f7d5c28098c25bd2a6f583f4a9bb8188a6962
LucaHaverty <lucahaverty@gmail.com>
2023-12-24 13:20:58 -0600

    Sooorta fixed materials, exhaust particles

--- Assets/Scripts/CombineHitEffect.cs
+++ Assets/Scripts/CombineHitEffect.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class CombineHitEffect : MonoBehaviour
 {
     [SerializeField] private Transform spawnPosition;
+    [SerializeField] private Transform particleParent;
 
     private void OnCollisionEnter(Collision collision)
     {
@@ -13,10 +14,10 @@ public class CombineHitEffect : MonoBehaviour
                 out AbstractCollidableObject collidableObject))
         {
             Destroy(Instantiate(Settings.instance.deathEffect,
-                    spawnPosition.position, spawnPosition.rotation, Settings.instance.effectsContainer), 2f
+                    spawnPosition.position, spawnPosition.rotation, transform), 2f
             );
             Destroy(Instantiate(Settings.instance.featherEffect,
-                    spawnPosition.position, spawnPosition.rotation, Settings.instance.effectsContainer), 2f
+                    spawnPosition.position, spawnPosition.rotation, transform), 2f
             );
         }
     }
